624c0d10fb0538037c3a2bbd5b0f37b3890e7d71
Sangam Kumar Singh <99814514+sangamk04@users.noreply.github.com>
2023-03-13 11:40:40 +0530

    ok

--- src/Components/DeleteItem.jsx
+++ src/Components/DeleteItem.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const DeleteItem = (props) => {
   return (
     
+    
     // <div style={{display:"flex",justifyContent:"space-around",alignItems:"center",margin:"auto", marginTop:"20px",width:"200px",border:"1px solid blue",background:"brown",}}>
     
     <div>
